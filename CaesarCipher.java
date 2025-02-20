f2cf4933342ff8f71810070dd15225a62314516e
rJohn0 <romeo.john@stu.daviess.kyschools.us>
2025-02-20 19:35:16 +0000

    wow i didnt need that else

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -6,31 +6,79 @@
  * @version 1.00 2025/2/20
  */
 
+import java.util.Arrays;
 
 public class CaesarCipher {
 
-	private char[][] alphabet;
-	private char[][] shifted;
+	private char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ,'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	private char[] shifted;
 	private int shift;
 
     public CaesarCipher() {
-
+    shifted = new char [26];
+    shift = 2;
+    
     }
 
     public CaesarCipher(int num){
-
+     shifted = new char [26];
+     shift = num;
+     shifter(shift);
     }
 
     public String encrypt(String message){
+        String outputEncrypt = "";
 
+        //go through message position
+        for(int i =0; i<message.length(); i++)
+        {
+            boolean found = false;
+            // go through alphabet position
+            for(int z =0; z<alphabet.length; z++)
+            {
+                if(message.charAt(i) == alphabet[z])
+                {
+                    found = true;
+                    outputEncrypt += shifted[z];
+                }
+            }
+            if(!found)
+              outputEncrypt += message.charAt(i);
+            
+        }
+     
+        return "" + outputEncrypt;
     }
 
     public String decrypt(String message){
+        String outputDecrypt = "";
 
+        for(int i =0; i<message.length(); i++)
+        {
+            boolean found = false;
+            // go through alphabet position
+            for(int z =0; z<alphabet.length; z++)
+            {
+                if(message.charAt(i) == shifted[z])
+                {
+                    found = true;
+                    outputDecrypt += alphabet[z];
+                }
+            }
+            if(!found)
+            outputDecrypt += "" + message.charAt(i);
+            
+        }
+    
+        return "" + outputDecrypt;
     }
 
     public void shifter(int num){
-
+        for(int i = 0; i<alphabet.length; i++)
+        {
+            shifted[(i+num)%26] = alphabet[i];
+        }
+        System.out.println(Arrays.toString(shifted));
     }
 
 
--- G19.java
+++ G19.java
@@ -6,7 +6,7 @@
  * @version 1.00 2025/2/20
  */
 
-
+import java.util.Arrays;
 public class G19 {
 
     public static void main(String[] args) {
@@ -15,9 +15,13 @@ public class G19 {
 
     	System.out.println(cipher.encrypt("hello world!"));
     	System.out.println(cipher.decrypt("ukq zez ep!"));
-
+		
     	//Create a new cipher using the shifter(#) method
     	//Encrypt and decrypt a message with your new cipher
+
+		cipher.shifter(8);
+		System.out.println(cipher.encrypt("i dont like timed frqs"));
+		System.out.println(cipher.decrypt("a vav al!"));
 
     }
 
